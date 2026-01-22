8e2752cef9e3c78f09b321ce8a2dc5fca4c8898b
fdcirettg <federico.cirett@unison.mx>
2026-01-21 18:51:16 -0700

    Boards is working

--- tictactoe/game_logic/board.py
+++ tictactoe/game_logic/board.py
@@ -8,7 +8,19 @@ def display_board(dboard:dict)->None:
     """
     d = dboard
     print(f"{d[0]:2s}|{d[1]:2s}|{d[2]:2s}")
-    print("---+---+---")
+    print("--+--+--")
     print(f"{d[3]:2s}|{d[4]:2s}|{d[5]:2s}")
-    print("---+---+---")
+    print("--+--+--")
     print(f"{d[6]:2s}|{d[7]:2s}|{d[8]:2s}")
+
+def player_turn(player:str, dboard:dict)->int:
+    """ Ask player for their turn
+    """
+
+
+if __name__ == "__main__":
+    board = {x:str(x) for x in range(9)}
+    display_board(board)
+    board[0] = 'X'
+    board[4] = 'O'
+    display_board(board)
