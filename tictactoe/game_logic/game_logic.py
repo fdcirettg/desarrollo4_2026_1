fb3cc0079c495d851a3c8fae2fcb68eed489aa31
fdcirettg <federico.cirett@unison.mx>
2026-01-29 18:40:20 -0700

    Moved two_players() to game_logic.py and added main menu

--- tictactoe/game_logic/game_logic.py
+++ tictactoe/game_logic/game_logic.py
@@ -50,6 +50,25 @@ def game()->str:
     #else:
     #    print(f"It's a tie!")
 
+def two_players():
+    """ Two players game loop
+    """
+    playing = True
+    score = {'X':0, 'O':0, 'Ties':0}
+    while playing:
+        winner = game()
+        if len(winner) > 0:
+            print(f"Winner: Player {winner}")
+        else:
+            print("It's a tie!")
+            winner = 'Ties'
+        score[winner] += 1
+        replay = input("Do you want to play again? (y/n): ").strip().lower()
+        if replay != 'y':
+            playing = False
+        print(f"Score: X = {score['X']}, O = {score['O']}, Ties = {score['Ties']}")
+
+
 if __name__ == "__main__":
     win = game()
     if len(win) > 0:
