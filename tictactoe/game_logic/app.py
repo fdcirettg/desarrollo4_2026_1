66128db8d90d0fc1b765e9c7cad305083382c38c
fdcirettg <federico.cirett@unison.mx>
2026-01-29 19:39:55 -0700

    Removed import from game_logic

--- tictactoe/game_logic/app.py
+++ tictactoe/game_logic/app.py
@@ -2,25 +2,25 @@
 Tic Tac Toe Game
 Author: Federico Cirett Galan
 """
-from game_logic import game
+#from game_logic import game
+from game_logic import two_players
+from menu import display_menu
 
 def main():
-    """ Main function to start the game 
+    """ Main function to run the Tic Tac Toe game
     """
-    playing = True
-    score = {'X':0, 'O':0, 'Ties':0}
-    while playing:
-        winner = game()
-        if len(winner) > 0:
-            print(f"Winner: Player {winner}")
+    while True:
+        choice = display_menu()
+        if choice == 1:
+            print("One Player Game is not implemented yet.")
+            # Here you would call the one player game function when implemented
+        elif choice == 2:
+            two_players()
+        elif choice == 3:
+            print("Exiting the game. Goodbye!")
+            break
         else:
-            print("It's a tie!")
-            winner = 'Ties'
-        score[winner] += 1
-        replay = input("Do you want to play again? (y/n): ").strip().lower()
-        if replay != 'y':
-            playing = False
-        print(f"Score: X = {score['X']}, O = {score['O']}, Ties = {score['Ties']}")
+            print("Invalid choice. Please select a valid option.")
         
 if __name__ == "__main__":
     main()
