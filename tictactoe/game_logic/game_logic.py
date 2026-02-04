10a6669ce7f9c8e6c0aa51575676bbfe23037030
fdcirettg <federico.cirett@unison.mx>
2026-02-03 18:52:49 -0700

    One and Two player functionality

--- tictactoe/game_logic/game_logic.py
+++ tictactoe/game_logic/game_logic.py
@@ -3,8 +3,8 @@ Docstring for game_logic
 Author: Federico Cirett Galán
 Here goes the game logic for Tictactoe
 """
-import board
 import random
+import board
 
 def check_winner(d:dict, combo_list:list)->bool:
     """
@@ -15,7 +15,7 @@ def check_winner(d:dict, combo_list:list)->bool:
             return True
     return False
 
-def game()->str:
+def game(num_players:int)->str:
     """
     Here lives the main game loop
     """
@@ -33,9 +33,11 @@ def game()->str:
     w_player = ""
     while turns < 9 and not winner:
         board.display_board(dboard)
-        valid_move = False
-        while not valid_move:
-            valid_move = board.player_turn(current_player, dboard)
+        if num_players == 1:
+            one_player(current_player, dboard)
+        else:
+            if num_players == 2:
+                two_player(current_player, dboard)
         turns += 1
         winner = check_winner(dboard, combo_list)
         if winner:
@@ -56,11 +58,8 @@ def play_game(players=2)->None:
     """
     playing = True
     score = {'X':0, 'O':0, 'Ties':0}
-    while playing:
-        if players == 1: # one player
-            winner = game_vs_computer()
-        else:          # two players
-            winner = game()
+    while playing: 
+        winner = game(players)
         if len(winner) > 0:
             print(f"Winner: Player {winner}")
         else:
@@ -71,45 +70,30 @@ def play_game(players=2)->None:
         if replay != 'y':
             playing = False
         print(f"Score: X = {score['X']}, O = {score['O']}, Ties = {score['Ties']}")
-   
-def game_vs_computer()->str:
-    """ Game vs computer loop
+
+def one_player(current_player:str, dboard:dict)->None:
+    """ One player vs computer game loop
     """
-    turns = 0
-    dboard = {x:str(x) for x in range(9)}
-    combo_list = [
-        [0,1,2], [3,4,5], [6,7,8], # rows
-        [0,3,6], [1,4,7], [2,5,8], # columns
-        [0,4,8], [2,4,6]           # diagonals  
-    ]
+    valid_move = False
     x_player = 'X'
     o_player = 'O'
-    current_player = x_player
-    w_player = ""
-    winner = False
-    while turns < 9 and not winner:
-        board.display_board(dboard)
-        valid_move = False
-        if current_player == x_player:
-            while not valid_move:
-                valid_move = board.player_turn(current_player, dboard)
-        else:
-            print("Computer's turn:")
-            while not valid_move:
-                move = random.randint(0,8)
-                if str(dboard[move]) == str(move):
-                    dboard[move] = o_player
-                    valid_move = True
-        turns += 1
-        winner = check_winner(dboard, combo_list)
-        if winner:
-            w_player = current_player
-        if current_player == x_player:
-            current_player = o_player
-        else:
-            current_player = x_player
-    board.display_board(dboard)
-    return w_player
+    if current_player == x_player:
+        while not valid_move:
+            valid_move = board.player_turn(current_player, dboard)
+    else:
+        print("Computer's turn:")
+        while not valid_move:
+            move = random.randint(0,8)
+            if str(dboard[move]) == str(move):
+                dboard[move] = o_player
+                valid_move = True
+
+def two_player(current_player:str, dboard:dict)->None:
+    """ Two players game loop"""
+    valid_move = False
+    while not valid_move:
+        valid_move = board.player_turn(current_player, dboard)
 
 if __name__ == "__main__":
     play_game(1)
+    
