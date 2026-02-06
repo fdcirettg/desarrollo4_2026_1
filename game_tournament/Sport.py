0378c800b22a77bc1742d6b8f60e61cd4c249ebe
fdcirettg <federico.cirett@unison.mx>
2026-02-05 18:46:31 -0700

    Started the Sport class

---
+++ game_tournament/Sport.py
@@ -0,0 +1,7 @@
+""" Sport class represents a sport in the tournament. It has a name and a league. """
+class Sport:
+    """ Sport class represents a sport in the tournament. It has a name and a league. """
+    def __init__(self, name, num_players, league):
+        """ Custom constructor for Sport class. """
+        self.name = name
+        self.league = league
