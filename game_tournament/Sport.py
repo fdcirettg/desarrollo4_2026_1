4dc5418be4099b3fef88146c615e24dd05f9c433
fdcirettg <federico.cirett@unison.mx>
2026-02-06 18:26:33 -0700

    Finished Sport class

--- game_tournament/Sport.py
+++ game_tournament/Sport.py
@@ -5,3 +5,23 @@ class Sport:
         """ Custom constructor for Sport class. """
         self.name = name
         self.league = league
+        self.num_players = num_players
+    def __str__(self):
+        """ String representation of the Sport class. """
+        return f"{self.name} ({self.league}) - {self.num_players} players"
+    def __repr__(self):
+        """ String representation of the Sport class. """
+        return f"Sport(name={self.name}, league={self.league}), players={self.num_players})"
+    def to_json(self):
+        """ Convert the Sport object to a JSON string. """
+        return {
+            "name": self.name,
+            "league": self.league,
+            "num_players": self.num_players
+        }
+
+if __name__ == "__main__":
+    sport = Sport("Basketball", 10, "NBA")
+    print(sport)
+    print(sport.to_json())
+    print(repr(sport))
